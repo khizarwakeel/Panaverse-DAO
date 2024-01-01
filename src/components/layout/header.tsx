306c928eb9f89d0f2ce4a34f64773e63457af4df
Khizar Wakeel <khizarwakeel@gmail.com>
2024-01-01 23:42:22 +0500

    added drop down

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,12 +1,26 @@
 "use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import Logo from "public/assets/logo.png";
 import Wrapper from "../shared/wrapper";
 import { usePathname } from "next/navigation";
+import { Fragment, useState } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+
+const quarters = [
+  "Web 3.0 and Metaverse Developer",
+  "Artificial Intelligence",
+  "Cloud-Native Computing",
+  "Ambient Computing and IoT",
+  "Genomics and Bioinformatics",
+  "Network Programmability and Automation",
+];
 
 const Header = () => {
   const pathName = usePathname();
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
   return (
     <div className="bg-gray-50 sticky top-0 bg-opacity-90 backdrop-blur-md z-50">
       <Wrapper>
@@ -24,12 +38,43 @@ const Header = () => {
               </Link>
             </li>
             <li>
-              <Link
-                href={"/courses"}
-                className={`${pathName === "/courses" ? "text-[#00636d]" : ""}`}
+              <div
+                onMouseEnter={() => setIsPopoverOpen(true)}
+                onMouseLeave={() => setIsPopoverOpen(false)}
               >
-                Courses
-              </Link>
+                <Popover className="relative">
+                  <Popover.Button className="inline-flex outline-none items-center gap-x-1 font-semibold leading-6 text-gray-900">
+                    <span>Courses</span>
+                    <ChevronDownIcon className="h-5 w-5" aria-hidden="true" />
+                  </Popover.Button>
+                  <Transition
+                    as={Fragment}
+                    show={isPopoverOpen}
+                    enter="transition ease-out duration-300"
+                    enterFrom="opacity-0 translate-y-1"
+                    enterTo="opacity-100 translate-y-0"
+                    leave="transition ease-in duration-300"
+                    leaveFrom="opacity-100 translate-y-0"
+                    leaveTo="opacity-0 translate-y-1"
+                  >
+                    <Popover.Panel
+                      onMouseEnter={() => setIsPopoverOpen(true)}
+                      onMouseLeave={() => setIsPopoverOpen(false)}
+                      className="absolute z-10 mt-3 flex right-0"
+                    >
+                      <div className="flex-auto w-80 overflow-hidden rounded-sm bg-white text-sm text-gray-500 border">
+                        <div className="p-3">
+                          {quarters.map((item) => (
+                            <div>
+                              <p className="py-2">{item}</p>
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                    </Popover.Panel>
+                  </Transition>
+                </Popover>
+              </div>
             </li>
           </ul>
         </header>
